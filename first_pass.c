325a06843e74dfef50c11b118a8bb7a0d0d6bb4f
Tomer Paik <tomer.paikowsky@gmail.com>
2024-08-12 17:22:46 +0300

    start first pass

--- first_pass.c
+++ first_pass.c
@@ -1,47 +1,30 @@
 #include "first_pass.h"
 
 int first_pass(char * am_path) {
-    int error_code;
-    int line_num;
     FILE * am_file;
-    char line[MAX_LINE_LENGTH];
-    char first_word[MAX_LINE_LENGTH];
-    char line_copy[MAX_LINE_LENGTH];
+    char line[MAX_LINE_LENGTH], first_word[MAX_LINE_LENGTH], symbol_name[MAX_LINE_LENGTH];
+    int error = 0;
+    int symbol_flag = 0, offset = 0;
     am_file = open_new_file(am_path, ".am", "r");
-    line_num = 0;
-    if (!check_line_length(am_path,".am", MAX_LINE_LENGTH)) {
-        return 0;
-    }
 
     /* Initialize  symbol table */
-    /*TODO: check first if there is a line with more than Max characters*/
 
     while (fgets(line, MAX_LINE_LENGTH, am_file)) {
-        line_num ++;
-        if (strcmp(line, "\n") == 0 || is_comment(line)) { /*blank line*/
-            continue;
-        }
-        get_first_word(line, first_word);
+        /*initial variables*/
+        symbol_flag = 0;
+        error = 0;
 
-        if (strchr(line, '.')) { /*instructions*/
-            strcpy(line_copy, line);
-            if (strstr(line_copy, ".data") != NULL || strstr(line_copy, ".string") != NULL){
-                if (legal_label(first_word, &error_code)) { /*lable*/
-                    /*insert_table(sym_table, first_word, "");*/
-                    /*insert_label(symbols, first_word, DC, 1);*/
-                }
-            }else if (strstr(line_copy, ".entry") || strstr(line_copy, ".extern")) {
-                printf("extern/entry\n");
-            }else {
-                /*Illegal data line directive - must be .data or .string or .extern or . entry */
-                error_code = ERROR_CODE_58;
+        sscanf(line, "%s%n", first_word, &offset);
+
+        if(is_symbol(first_word, &error)) {
+            if(error) {
+                /* TODO: print_error(error); */
+                continue;
             }
-
-
-        }else { /*commands*/
-            continue;
+            strcpy(symbol_name, first_word);
+            symbol_flag = 1;
+            sscanf(line+offset, "%s", first_word);
         }
-
     }
 
     fclose(am_file);
@@ -52,4 +35,31 @@ int first_pass(char * am_path) {
 
     return 1;
 
+}
+
+int is_symbol(const char *word, int *error) {
+    int word_len = strlen(word);
+    /*TODO: chek if word is a symbol*/
+    if (word[word_len - 1] != ':') {
+        return 0;
+    }
+    /*TODO: chek valid len*/
+    if(word_len > 32){
+        *error = 1;
+    }
+    /*TODO: check symbol start with a letter*/
+    if (!isalpha(word[0])) {
+        *error = 1;
+    }
+    /*TODO: chek if char in symbol is valid*/
+    for (int i = 1; i < word_len - 1; i++) {
+        if (!isalnum(word[i]) && word[i] != '_') {
+            *error = 1;
+            return 0;
+        }
+    }
+
+
+
+    return 1;
 }
--- first_pass.h
+++ first_pass.h
@@ -5,4 +5,5 @@
 #include "lexer.h"
 #include "hash_table.h"
 int first_pass(char * am_path);
+int is_symbol(const char *word, int *error);
 #endif
